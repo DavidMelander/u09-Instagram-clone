fda7c5b2cb5df334442fc6e4ef067ac3ee8aa395
David Melander <david.melander@chasacademy.se>
2022-10-20 16:11:53 +0200

    First commit, Navbar finish

---
+++ components/Stories.js
@@ -0,0 +1,18 @@
+import faker from 'faker'
+
+import React, { useEffect } from 'react'
+
+function Stories() {
+
+
+  return (
+    <div>
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+    </div>
+  )
+}
+
+export default Stories
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Feed from '../components/Feed'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
     {/* Header */}
     <Header />
     {/* Feed */}
+    <Feed />
 
     {/* Model */}
     </div>
