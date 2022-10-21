74f7fc6de32c58d10633e00edfc703c4612acc5e
David Melander <david.melander@chasacademy.se>
2022-10-21 07:25:13 +0200

    Stories component frontend finished

--- components/Stories.js
+++ components/Stories.js
@@ -1,12 +1,30 @@
 import faker from 'faker'
-
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Story from './Story'
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
 
+
+   {/* FAKE DATA */}
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+
+    setSuggestions(suggestions);
+    console.log(suggestions);
+  }, [])
 
   return (
-    <div>
+    <div className='flex space-x-2 p-6 bg-white mt-8 broder-gray-200 border 
+    rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+      {suggestions.map(profile => (
+        <Story key={profile.id} 
+        img={'https://media.gq.com/photos/599db9f8021601733ff6a5f6/master/pass/the-joker-movie.jpg'} 
+        username={profile.username} />
+      ))}
       {/* Story */}
       {/* Story */}
       {/* Story */}
